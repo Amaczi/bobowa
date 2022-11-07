81759dfc6737140c39b57746502bdde058aeae30
Amaczi <Amaczi13@gmail.com>
2022-11-07 17:10:00 +0100

    update font location

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,6 @@ export default function App({ Component }) {
     <>
       <Head>
         <title>Bobowa24</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <Component />
       <Script src="https://unpkg.com/phosphor-icons" />
