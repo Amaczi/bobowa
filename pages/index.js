b69d855203aa956d452a23ae92153adfc8682b0a
Amaczi <Amaczi13@gmail.com>
2022-11-08 21:01:36 +0100

    Navbar implementation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 // Component imports
+import Navbar from "../files/components/Navbar/Navbar";
 import PostsWrapper from "../files/components/PostsWrapper/PostsWrapper";
 
 export default function Index() {
-  return <PostsWrapper />;
+  return (
+    <>
+      <Navbar />
+      <PostsWrapper />
+    </>
+  );
 }
