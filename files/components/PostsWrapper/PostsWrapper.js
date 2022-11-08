d3069b2d30e0c116101eeb1245daea596528dfa7
Amaczi <Amaczi13@gmail.com>
2022-11-08 18:40:46 +0100

    Category fetching fix

--- files/components/PostsWrapper/PostsWrapper.js
+++ files/components/PostsWrapper/PostsWrapper.js
@@ -22,34 +22,48 @@ export default function PostsWrapper({ categoryName }) {
   const [page, setPage] = useState(DEFAULT_PAGE);
   const [perpage, setPerPage] = useState(DEFAULT_PER_PAGE);
   const [searchPhrase, setSearchPhrase] = useState(DEFAULT_SEARCH);
-  const [categoryId, setCategoryId] = useState("");
+  const [categoryFetching, setCategoryFetching] = useState(true);
+  const [categoryId, setCategoryId] = useState(DEFAULT_CATEGORY);
 
   async function getCategoryId(name) {
-    if (name !== "undefined") {
+    categoryFetching === false && setCategoryFetching(true);
+    if (name !== "") {
       const data = await apiConnect(
         `https://bobowa24.pl/wp-json/wp/v2/categories/?slug=${name}`
       );
       if (data.posts[0] !== undefined) {
         setCategoryId(data.posts[0].id);
+        setCategoryFetching(false);
+      } else {
+        setCategoryFetching(false);
       }
+    } else {
+      setCategoryFetching(false);
     }
   }
 
   useEffect(() => {
     getCategoryId(dangerousData(categoryName || DEFAULT_CATEGORY));
+  }, [categoryName]);
+
+  useEffect(() => {
     loadData();
     scrollToTop();
-  }, [page, perpage, searchPhrase, categoryName, categoryId]);
+  }, [page, perpage, searchPhrase, categoryFetching]);
 
   async function loadData() {
-    const link = `https://bobowa24.pl/wp-json/wp/v2/posts/?per_page=${perpage}&page=${page}`;
-    let data = await apiConnect(
-      categoryId !== ""
-        ? `${link}&search=${searchPhrase}&categories=${categoryId}`
-        : `${link}&search=${searchPhrase}`
-    );
-    setMaxPages(data.maxPages);
-    setPosts(data.posts);
+    if (categoryFetching === false) {
+      const baseLink = `https://bobowa24.pl/wp-json/wp/v2/posts/?per_page=${perpage}&page=${page}`;
+      const linkPhrase = `&search=${searchPhrase}`;
+      const linkCategory = `&categories=${categoryId}`;
+      const link = `${baseLink}${searchPhrase !== "" ? linkPhrase : ""}${
+        categoryId !== "" ? linkCategory : ""
+      }`;
+
+      let data = await apiConnect(link);
+      setMaxPages(data.maxPages);
+      setPosts(data.posts);
+    }
   }
 
   return (
