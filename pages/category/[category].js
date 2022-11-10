8b34b63a3dde8ae5b0a981b20a6451a4f71b9bab
Amaczi <Amaczi13@gmail.com>
2022-11-10 12:55:57 +0100

    Adding navbar usage

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -3,11 +3,17 @@ import { useRouter } from "next/router";
 
 // Components imports
 import PostsWrapper from "../../files/components/PostsWrapper/PostsWrapper";
+import Navbar from "../../files/components/Navbar/Navbar";
 
 export default function () {
   const {
     query: { category },
   } = useRouter();
 
-  return <PostsWrapper categoryName={category} />;
+  return (
+    <>
+      <Navbar />
+      <PostsWrapper categoryName={category} />
+    </>
+  );
 }
